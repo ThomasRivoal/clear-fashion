098f54af062cc7e89629d72b362b9a44e7abe333
Thomas Rivoal <thomas.rivoal@edu.devinci.fr>
2022-03-21 15:33:06 +0100

    request

--- server/api.js
+++ server/api.js
@@ -2,6 +2,10 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+const {MongoClient} = require('mongodb');
+const MONGODB_URI = 'mongodb+srv://ThomasRiv:esilv@clearfashion.21byd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'clearfashion';
+
 const PORT = 8092;
 
 const app = express();
@@ -18,6 +22,51 @@ app.get('/', (request, response) => {
   response.send({'ack': true});
 });
 
+async function Connect(){
+  client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+  console.log("Connection Successful");
+  db =  await client.db(MONGODB_DB_NAME);
+}
+
+app.get('/products', async(request, response) => {
+  await Connect();
+  const prod= await mongo.find();
+  response.send(prod);
+});
+
+app.get('/:id', async(request, response) => {
+  await Connect();
+  const idprod= await mongo.find({'_id':request.params.id});
+  response.send(idprod);
+});
+
+
+app.get('/products/:search', async(request, response) => {
+  await Connect();
+  const query= request.query
+  const toFind={}
+
+  if(request.query.price!=null)
+  {
+    toFind['price'] = parseFloat(request.query.price);
+  }
+  if(request.query.brand!= null)
+  {
+    toFind['brand'] = request.query.brand;
+  }
+    
+  searchprod= await mongo.find(toFind);
+
+  let limit=request.query.slice;
+  if(request.query.slice==null)
+  { limit=12}
+  searchprod=searchprod.slice(0,limit);
+  
+  response.send(searchprod);
+});
+
 app.listen(PORT);
 
 console.log(`📡 Running on port ${PORT}`);
+
+
